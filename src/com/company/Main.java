14a1b866e129db2542142c36357854490c66e108
Y0ni0 <eshcolsam@gmail.com>
2022-11-18 23:08:28 +0300

    partialConfidence

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,7 +10,7 @@ public class Main {
     //Encode a number
     public static int[] encodeNumber(int n){
         if(n<=1) return null;
-        List<Integer> primeFactors = new ArrayList();
+        ArrayList<Integer> primeFactors = new ArrayList();
 
         for(int i=0; i<n; i++){
             while(n%i==0) primeFactors.add(i);
@@ -117,6 +117,91 @@ public class Main {
        }
         return 1;
     }
+    //largest prime factor
+    public static int largestPrimeFactor2(int n){
+        if(n<=1) return 0;
+        int num = n;
+        while(num >1 ){
+            if(n % num ==0){
+                boolean isPrime = true;
+                for(int i=2 ; i<n; i++){
+                    if(num %i ==0 ){
+                        isPrime =false;
+                        break;
+                    }
+                }
+                if(isPrime) return num;
+            }
+            num--;
+        }
+        return 0;
+    }
+    //encode a number
+    public static int[] encodeNumber2(int n){
+        if(n<=1) return null;
+        ArrayList<Integer> primeFactors = new ArrayList<>();
+        for(int i=2; i<n; i++){
+            while(n%i==0){
+               primeFactors.add(i);
+               n= n/i;
+            }
+        }
+        if(n>1) primeFactors.add(n);
+        int[] arraysToReturn = new int[primeFactors.size()];
+        for(int i=0; i<primeFactors.size(); i++){
+            arraysToReturn[i] = primeFactors.get(i);
+        }
+        return arraysToReturn;
+    }
+
+    // magic array
+    //{The sum of the prime numbers in the array must equal the first element of the array}
+    //if there are no primes the first element must be zero
+    //there is a function named isPrime the returns 1 if its passed int is prime
+    public static int isMagicArray (int[] a){
+        int sum = 0;
+        if(a[0]== 0) return 1;
+        for(int i=0; i<a.length-1; i++){
+            boolean isPrime = true;
+            for(int j=2; j<a[i];j++){
+                if(a[i]%j ==0)
+                    isPrime = false;
+            }
+            if(isPrime && a[i]> 0)
+                sum+=a[i];
+        }if(sum==a[0]) return 1;
+        return 0;
+    }
+    public static int isComplete(int[] a){
+        int min=0;
+        int max=0;
+        int countCondition =0;
+        for(int i=0; i<a.length-1; i++){
+            if(a[i]% 2==0){
+                min = a[i];
+                max = a[i];
+                countCondition++;
+                break;
+            }
+        }
+       for(int j=0; j<a.length-1; j++){
+           if(a[j]%2==0){
+               if(min>a[j]) min =a[j];
+               if(max<a[j]) max =a[j];
+           }
+       }if(min!=max) {
+           countCondition++;
+           int y = min +1;
+           for(int k=0; k<a.length-1; k++){
+                if(y==a[k] && y<max)
+                    y++;
+           }
+           countCondition++;
+        }
+       if(countCondition ==3) return 1;
+       return 0;
+    }
+
 
     public static void main(String[] args) {
 //        Scanner scanner = new Scanner(System.in);
@@ -174,18 +259,19 @@ public class Main {
             System.out.println("Example 6: " + example6);
      */
         //Test cases
-        int example1 = isSequencedArray2(new int[] {1,2,3,4,5},1,5);
-        int example2 = isSequencedArray2(new int[] {1,3,4,2,5},1 ,5);
-        int example3 = isSequencedArray2(new int[] {-5,-5,-4,-4,-3,-3,-2,-2,-2},-5,-2);
-        int example4 = isSequencedArray2(new int[] {0,1,2,3,4,5},1,5);
-        int example5 = isSequencedArray2(new int[] {1,2,3,4},1,5);
-        int example6 = isSequencedArray2(new int[] {1,2,5}, 1,5);
-        System.out.println("Example 1: " + example1);
+       // int example1 = isMagicArray(new int[] {21, 3, 7, 9,11,4, 6} );
+        int example2 = isComplete(new int[] {5, 7, 9, 13});
+//        int example2 = isSequencedArray2(new int[] {1,3,4,2,5},1 ,5);
+//        int example3 = isSequencedArray2(new int[] {-5,-5,-4,-4,-3,-3,-2,-2,-2},-5,-2);
+//        int example4 = isSequencedArray2(new int[] {0,1,2,3,4,5},1,5);
+//        int example5 = isSequencedArray2(new int[] {1,2,3,4},1,5);
+//        int example6 = isSequencedArray2(new int[] {1,2,5}, 1,5);
+        //System.out.println("Example 1: " + example1);
         System.out.println("Example 2: " + example2);
-        System.out.println("Example 3: " + example3);
-        System.out.println("Example 4: " + example4);
-        System.out.println("Example 5: " + example5);
-        System.out.println("Example 6: " + example6);
+//        System.out.println("Example 3: " + example3);
+//        System.out.println("Example 4: " + example4);
+//        System.out.println("Example 5: " + example5);
+//        System.out.println("Example 6: " + example6);
 //        System.out.println(checkConcatenatedSum(198, 2));
 //        System.out.println(checkConcatenatedSum(198, 3));
 //        System.out.println(checkConcatenatedSum(2997, 3));
