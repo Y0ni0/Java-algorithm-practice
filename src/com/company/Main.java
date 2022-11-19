2f0b387fdf0a66c71a35a77e3224c111d32a9a7e
Y0ni0 <eshcolsam@gmail.com>
2022-11-19 08:05:17 +0300

    act test day

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -201,7 +201,26 @@ public class Main {
        if(countCondition ==3) return 1;
        return 0;
     }
+    public static int isPrimeProduct(int n){
+        for(int i=2;i<n;i++){
+            int num1;
+            int num2;
+            if(n%i==0) {
+                num1 = n/i;
+                num2 = i;
+                boolean isPrime = true;
+                if(num1>1){
+                    for(int j=2;j<n;j++){
+                        if(num1%2==0) isPrime=false;
+                        break;
+                    }
+                }
+                if(isPrime) return 1;
+            }
 
+        }
+        return 0;
+    }
 
     public static void main(String[] args) {
 //        Scanner scanner = new Scanner(System.in);
