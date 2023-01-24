4e6a73c6ae17c8d1a541a9f53bc31807155215f1
Y0ni0 <eshcolsam@gmail.com>
2023-01-24 14:04:22 +0300

    interface implementation complete

--- src/com/company/Lamborghini.java
+++ src/com/company/Lamborghini.java
@@ -13,11 +13,29 @@ public class Lamborghini implements Cars {
 
     @Override
     public void applyBrake(int decrement) {
-
+        speed = speed - decrement;
     }
 
     @Override
     public void speedUp(int increment) {
-
+        speed = speed + increment;
+    }
+    public int getTyreHeight(){
+        return tyreHeight;
+    }
+    public void setTyreHeight(int tyreHeight){
+        this.tyreHeight = tyreHeight;
+    }
+    public int getSpeed(){
+        return speed;
+    }
+    public void setSpeed(int speed){
+        this.speed = speed;
+    }
+    public int getGear(){
+        return gear;
+    }
+    public void setGear(int gear){
+        this.gear = gear;
     }
 }
